a73fff3a32a6684ed0bad1cf5b3e39be913709de
berdiyorova <rano.baxromovna@gmail.com>
2024-12-16 10:00:49 +0500

    changed

--- Config/settings.py
+++ Config/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'Config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config("DB_NAME"),
+        'NAME': config("DB_NAME", default="yandex_eats"),
         'HOST': config("DB_HOST"),
         'PORT': config("DB_PORT"),
         'USER': config("DB_USER"),
